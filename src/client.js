465acbfcd7e33ea6a1a291d61ae0cef043fd3ad8
stf <sstefanofm@gmail.com>
2022-07-29 04:00:16 -0300

    Removed nodemon. Implemented arg dependency for cli arguments;

--- src/client.js
+++ src/client.js
@@ -1,31 +1,40 @@
-const username = process.argv[2];
+const args = require("arg")({
+  "--username": String,
+  "--address": String,
+
+  // Aliases;
+  "-u": "--username",
+  "-a": "--address",
+});
+
+if (!args["--username"])
+  return console.error("Missing required argument: --username");
+if (!args["--address"])
+  return console.error("Missing required argument: --address");
+
+const username = args["--username"];
+const address = args["--address"];
 
 const { input } = require("./utils/prompt");
 
-if (username) {
-  require("dotenv").config();
+const net = require("net");
 
-  const net = require("net");
+const client = new net.Socket();
 
-  const client = new net.Socket();
+client.connect(3030, address, () => {
+  client.write(`Hello, server! I'm ${username}`);
+});
 
-  client.connect(3030, process.env.ADDRESS, () => {
-    client.write(`Hello, server! I'm ${username}`);
-  });
+client.on("data", (data) => {
+  console.log(data.toString());
+});
 
-  client.on("data", (data) => {
-    console.log(data.toString());
-  });
+input(username, client);
 
-  input(username, client);
+client.on("close", () => {
+  console.log("Connection closed.");
+});
 
-  client.on("close", () => {
-    console.log("Connection closed.");
-  });
-
-  client.on("error", (error) => {
-    console.log(`There was an error: ${error}`);
-  });
-} else {
-  console.log("Please enter a username: `npm run client <username>`");
-}
+client.on("error", (error) => {
+  console.log(`There was an error: ${error}`);
+});
